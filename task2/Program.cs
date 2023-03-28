b704978e8a3df8a39174487781267f9764b5dac5
akulich87 <amgrupp_project@mail.ru>
2023-03-28 19:31:30 +0300

    add task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -16,3 +16,19 @@ while(num > 0)
 }
 
 Console.WriteLine($"Сумма цифр введенного числа: " +sum);
+
+
+// Решение(сторонний вариант, цикл for)
+
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+
+// int sum = 0;
+// for(int i = 0; i < number.Length; i++)
+// {
+//   string str = Convert.ToString(number[i]);
+//   int num1 = Convert.ToInt32(str);
+//   sum = sum + num1;
+// }
+
+// Console.WriteLine(sum);
